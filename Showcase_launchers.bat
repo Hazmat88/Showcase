05ec67b74c5eb641293faa7d6146c255ed80b56a
Hazougit88 <matthieu.vinet.hz@gmail.com>
2019-11-27 20:01:58 +0100

    Add Documents files CV

---
+++ Showcase_launchers.bat
@@ -0,0 +1,3 @@
+start cmd.exe /k "cd C:\Users\Moshka\Desktop\MM_P\Website\Showcase & compass watch"
+start cmd.exe /k "cd C:\Users\Moshka\Desktop\MM_P\Website\Showcase & browser-sync start --server --files 'stylesheets/*.css, js/*.js, *.html' "
+start cmd.exe /k "cd C:\Users\Moshka\Desktop\MM_P\Website\Showcase"
